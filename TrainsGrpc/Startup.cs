7077db78fd34c46d8a783679035e1c985d41ffe2
Deivydas Voroneckis <deivydas@atlantisgames.lt>
2020-10-20 16:50:52 +0300

    Small improvement

--- TrainsGrpc/Startup.cs
+++ TrainsGrpc/Startup.cs
@@ -17,7 +17,7 @@ namespace TrainsGrpc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ITrainsStorageService, TrainsStorageService>();
+            services.AddSingleton<ITrainsStorageService, TrainsStorageService>();
 
             services.AddGrpc();
 
